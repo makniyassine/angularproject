5c2ca9d3e911c357864d57a06108cebd82a43bf5
makniyassine <makniyassine20@gmail.com>
2020-12-30 19:33:50 +0100

    first commit

---
+++ helloword/src/app/regester/regester.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-regester',
+  templateUrl: './regester.component.html',
+  styleUrls: ['./regester.component.css']
+})
+export class RegesterComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
